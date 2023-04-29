f01188718a29af39134dabdebebe8080080abb3f
Anthony Cruz <Anthonyleierlw@gmail.com>
2023-04-29 11:55:19 -0300

    GET, PUT e DELETE de Empréstimos e Pagamentos

--- onedata/urls.py
+++ onedata/urls.py
@@ -1,21 +1,21 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from onedata.views import EmprestimoListCreate
-from onedata.views import PagamentoListCreate
+from onedata.views import EmprestimosListarCriar, EmprestimoDetalhar
+from onedata.views import PagamentosListarCriar, PagamentoDetalhar
 
 urlpatterns = [
     # Obter o token de autenticação
     path('token/', obtain_auth_token, name='auth_token'),
 
     # Criar um novo empréstimo ou listar todos os empréstimos
-    path('emprestimos/', EmprestimoListCreate.as_view(), name='criar_lista_emprestimos'),
+    path('emprestimos/', EmprestimosListarCriar.as_view(), name='criar_listar_emprestimos'),
 
     # # Obter detalhes de um empréstimo específico
-    # path('emprestimos/<int:pk>/', EmprestimoDetail.as_view(), name='detalhar_emprestimos'),
+    path('emprestimos/<int:pk>/', EmprestimoDetalhar.as_view(), name='detalhar_emprestimo'),
 
     # Criar um novo pagamento e listar todos os pagamentos
-    path('pagamentos/', PagamentoListCreate.as_view(), name='criar_listar_pagamentos'),
+    path('pagamentos/', PagamentosListarCriar.as_view(), name='criar_listar_pagamentos'),
 
     # # Obter detalhes de um pagamento específico
-    # path('pagamentos/<int:pk>/', PagamentoDetail.as_view(), name='detalhar_pagamento'),
+    path('pagamentos/<int:pk>/', PagamentoDetalhar.as_view(), name='detalhar_pagamento'),
 ]
--- onedata/views.py
+++ onedata/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,7 +11,7 @@ from onedata.serializers import EmprestimoSerializer, PagamentoSerializer
 from onedata.utils import get_ip_usuario
 
 
-class EmprestimoListCreate(APIView):
+class EmprestimosListarCriar(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
@@ -31,7 +32,29 @@ class EmprestimoListCreate(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class PagamentoListCreate(APIView):
+class EmprestimoDetalhar(APIView):
+    def get(self, request, pk):
+        emprestimo = get_object_or_404(klass=Emprestimo, pk=pk, usuario=request.user)
+        serializer = EmprestimoSerializer(instance=emprestimo, many=False)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        emprestimo = get_object_or_404(klass=Emprestimo, pk=pk, usuario=request.user)
+        serializer = EmprestimoSerializer(instance=emprestimo, data=request.data, many=False)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        emprestimo = get_object_or_404(klass=Emprestimo, pk=pk, usuario=request.user)
+        emprestimo.delete()
+        return Response(f"Empréstimo {pk} removido com sucesso", status=status.HTTP_204_NO_CONTENT)
+
+
+class PagamentosListarCriar(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
@@ -49,3 +72,25 @@ class PagamentoListCreate(APIView):
             return Response(request.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PagamentoDetalhar(APIView):
+    def get(self, request, pk):
+        pagamento = get_object_or_404(klass=Pagamento, pk=pk, usuario=request.user)
+        serializer = PagamentoSerializer(instance=pagamento, many=False)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        pagamento = get_object_or_404(klass=Pagamento, pk=pk, usuario=request.user)
+        serializer = PagamentoSerializer(instance=pagamento, data=request.data, many=False)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        pagamento = get_object_or_404(klass=Pagamento, pk=pk, usuario=request.user)
+        pagamento.delete()
+        return Response(f"Pagamento {pk} removido com sucesso", status=status.HTTP_204_NO_CONTENT)
