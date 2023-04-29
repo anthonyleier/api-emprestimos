eff7d4088091a772bcb68328e6977cc0ce5adbc7
Anthony Cruz <Anthonyleierlw@gmail.com>
2023-04-29 13:32:35 -0300

    Ajustes nos campos dos serializers

--- onedata/serializers.py
+++ onedata/serializers.py
@@ -1,18 +1,17 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from onedata.models import Emprestimo, Pagamento
+
+
+class EmprestimoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Emprestimo
+        fields = ['id', 'valor_nominal', 'taxa_juros', 'ip', 'data', 'banco', 'cliente', 'usuario']
+        read_only_fields = ['id', 'data']
 
 
 class PagamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pagamento
         fields = ['id', 'emprestimo', 'valor', 'data', 'usuario']
-        read_only_fields = ['id', 'data']
-
-
-class EmprestimoSerializer(serializers.ModelSerializer):
-    pagamentos = PagamentoSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Emprestimo
-        fields = ['id', 'valor_nominal', 'taxa_juros', 'ip', 'data', 'banco', 'cliente', 'pagamentos', 'usuario']
         read_only_fields = ['id', 'data']
