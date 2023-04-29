b071129ce4f8b1e52f3eeb32184cb6287d4ae906
Anthony Cruz <Anthonyleierlw@gmail.com>
2023-04-29 19:06:12 -0300

    Registros das models para o admin

--- onedata/admin.py
+++ onedata/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from onedata.models import Emprestimo, Pagamento
 
-# Register your models here.
+
+@admin.register(Emprestimo)
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'valor_nominal', 'taxa_juros', 'ip', 'data', 'banco', 'cliente', 'usuario']
+    list_display_links = ['id']
+    list_filter = ['usuario', 'data']
+    search_fields = ['banco', 'cliente']
+    list_per_page = 20
+    ordering = ['-id']
+
+
+@admin.register(Pagamento)
+class PagamentoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'emprestimo', 'valor', 'data', 'usuario']
+    list_display_links = ['id']
+    list_filter = ['usuario', 'data']
+    list_per_page = 20
+    ordering = ['-id']
